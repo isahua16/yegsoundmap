37852fd1df894131bb2eba3f0711c6fbc8778e84
isahua16 <32420364+isahua16@users.noreply.github.com>
2022-12-07 22:13:29 -0700

    Added the functionality to retrieve the data from the database to the map

---
+++ load_poi.php
@@ -0,0 +1,23 @@
+<?php
+    $db = new PDO("pgsql:host=localhost;port=5432;dbname=yegsoundmap;", "postgres","isahua9261");
+
+    $sql = $db->query("SELECT id, name, audio, ST_AsGeoJSON(geom,5) as geom FROM yeg_poi ORDER BY id");
+
+    $features = [];
+
+    while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+
+        $feature=["type"=>"Feature"];
+        
+        $feature["geometry"]=json_decode($row["geom"]);
+
+        unset($row["geom"]);
+
+        $feature["properties"]=$row;
+
+        array_push($features, $feature);
+    }
+
+    $featureCollection=["type"=>"FeatureCollection", "features"=>$features];
+    echo json_encode($featureCollection);
+?>
