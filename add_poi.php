dba8cb84cacbac050e80c2022151217276a0aa71
isahua16 <32420364+isahua16@users.noreply.github.com>
2022-12-13 13:06:31 -0700

    Upload system refactor
    - The system now validates the data to see if it is a wav, mp3 or ogg, is 50MB or less, that it does not have an upload error.
    - The system now also creates a new filename for the uploaded audio, which makes sure that there will be no conflicts where a client might upload a generic audio name, ie. audio.wav, and have the sound not be assigned to the right row.
    - The new file name is also getting populated in the database so that it gets retrieved on load_poi.php.

--- add_poi.php
+++ add_poi.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_FILES["poi"])) {
+if (isset($_FILES["poi"]) && $_FILES['poi']['error'] === UPLOAD_ERR_OK) {
 
     $fileTmpPath = $_FILES['poi']['tmp_name'];
     $fileName = $_FILES['poi']['name'];
@@ -8,15 +8,29 @@ if (isset($_FILES["poi"])) {
     $fileType = $_FILES['poi']['type'];
     $fileNameCmps = explode(".", $fileName);
     $fileExtension = strtolower(end($fileNameCmps));
+    $newFileName = round(microtime(true)) . '.' . $fileExtension;
 
-    $uploadFileDir = 'media/';
-    $dest_path = $uploadFileDir . $fileName;
+    $allowedExtensions = array('wav', 'mp3', 'ogg');
 
-    if(move_uploaded_file($fileTmpPath, $dest_path)) 
-      {
-        echo 'File is successfully uploaded.';
-      }
-}
+    if (in_array($fileExtension, $allowedExtensions) && $fileSize < 50000000) {
+        
+        $uploadFileDir = 'media/';
+        $dest_path = $uploadFileDir . $newFileName;
+        
+            if(move_uploaded_file($fileTmpPath, $dest_path)) 
+            {
+                echo 'File was successfully uploaded to the server.';
+            } else {
+                echo 'There was an error moving the file to the server directory';
+            }
+         }  else { 
+            echo 'Upload failed. Please make sure that your file is in either .wav, .mp3 or .ogg format, and less than 20MB.';
+        }
+    } else {
+        $message = 'There is some error in the file upload.';
+        $message .= 'Error:' . $_FILES['poi']['error'];
+        echo $message;
+    }
 
 if (isset($_POST["latitude"]) && is_numeric($_POST["latitude"])) {
     $latitude=$_POST["latitude"];
@@ -37,15 +51,10 @@ if (isset($_POST["name"])) {
 }
 
 if (isset($_POST["audio"])) {
-    $postAudio=$_POST["audio"];
-    $fakePath="C:\\fakepath\\";
-    $realPath = "media/";
-    $audio=str_replace($fakePath, $realPath, $postAudio);
+    $audio = $dest_path;
 } else {
     $audio="NA";
 }
-
-
 
 $db = new PDO("pgsql:host=localhost;port=5432;dbname=yegsoundmap;", "postgres","isahua9261");
 
@@ -53,9 +62,8 @@ $sql = $db->prepare("INSERT INTO yeg_poi (geom, name, audio) VALUES ((st_setsrid
 
 $params = ["nm"=>$name,"lat"=>$latitude,"lng"=>$longitude,"ad"=>$audio];
 
-
 if ($sql->execute($params)) {
-    echo "{$name} succesfully added";
+    echo "Your submission was succesfully added to the map.";
 } else {
     echo var_dump($sql->errorInfo());
 };
