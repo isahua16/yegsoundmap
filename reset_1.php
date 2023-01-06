16b53c1f50949f416e4822b1466106ac2bb4f230
isahua16 <32420364+isahua16@users.noreply.github.com>
2023-01-05 19:18:16 -0700

    Update reset_1.php

--- reset_1.php
+++ reset_1.php
@@ -9,7 +9,7 @@
 
             $row = return_field_data($pdo, "users", "username", $username);
             
-            $body = "Please go to https://{$_SERVER['SERVER_NAME']}/reset_2.php?user={$username}&code={$row['validationcode']} to reset your password";
+            $body = "Please go to https://{$_SERVER['SERVER_NAME']}/reset_2.php?user={$username}&code={$row['validationcode']} to reset your password. If you did not initiate the password reset, you can ignore this message.";
                 
             send_mail($row['email'], "Reset Password", $body, $from_email, $reply_email);
             set_msg("Please check your email for the reset url. Don't forget to check your spam folder.");
