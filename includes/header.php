dd4d3cd4def84bc71f597db730e431ab68aa58a9
isahua16 <32420364+isahua16@users.noreply.github.com>
2023-01-05 20:42:23 -0700

    css updates

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <!-- Custom Styles -->
     <link rel="stylesheet" href="css/styles.css" />
