5c443863dd9371c39c62d9d61c2cd967156066da
isahua16 <32420364+isahua16@users.noreply.github.com>
2023-01-18 23:15:16 -0700

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -6,7 +6,7 @@
       
       echo "<li class='nav_item'>Hello, <a class='username' href='#'>{$username}</a>!</li>";
       
-      echo "<li class='nav_item' href='{$user_page}'><img class='logo' style='height:25, width:32' src='media/logo.png' alt='Edmonton Sound Map'/></li>";
+      echo "<li><a class='nav_item' href='{$user_page}'><img class='logo' src='media/logo.png' alt='Edmonton Sound Map'/></a></li>";
       
       echo "<li> <a class='nav_item' href='{$logout_page}'>Logout</a></li>";
       
